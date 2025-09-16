a8296f8d0970a6c75b36f6cf1cab66fcc2a8fcdd
Pedro Velázquez <pedro.velazquez@markethub.com.ar>
2025-09-16 14:34:21 -0300

    .

--- code/config_options.py
+++ code/config_options.py
@@ -1,6 +1,6 @@
 chosen_day = "2025-05-05"
 
 
-carpeta_md_opciones = r"C:\Users\Pedro\Research\options_project\market_data\Opciones"
+dir_md_opciones = r"C:\Users\Pedro\Research\options_project\market_data\Opciones"
 
-carpeta_md_subyacente=r"C:\Users\Pedro\Research\options_project\market_data\Subyacente"
+dir_md_subyacente=r"C:\Users\Pedro\Research\options_project\market_data\Subyacente"
