8dfc70e625e3664008eb8f1eedb4415c8985c5dc
Pedro Velázquez <pedro.velazquez@markethub.com.ar>
2025-09-24 17:09:05 -0300

    .

--- code/config_options.py
+++ code/config_options.py
@@ -1,9 +1,10 @@
-chosen_day = "2025-05-05"
+chosen_day = "2025-07-22"
 ddm_trading_stocks_pct =0.0700
 ddm_trading_opts_pct = 0.2000
 ddm_exercising_options_pct= 0.0800
 iva_pct = 0.21 
 multiplicador=100
+COOLDOWN_SECONDS = 3
 
 dir_md_opciones = r"C:\Users\Pedro\Research\options_project\market_data\Opciones"
 dir_md_subyacente=r"C:\Users\Pedro\Research\options_project\market_data\Subyacente"
